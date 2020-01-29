c6da60a9ae7d3c9d74071185745c6427731cbb09
laposata <34639022+laposata@users.noreply.github.com>
2020-01-29 14:23:30 -0500

    Update Array.h

--- Array.h
+++ Array.h
@@ -17,7 +17,7 @@ public:
 
 class objArray: public Array{
 public:
-  Array(size_t length, size_t data_length); // Creates an array of size length, where each object is data_length long
+  objArray(size_t length, size_t data_length); // Creates an array of size length, where each object is data_length long
   Object* get(size_t index); //returns the object at index
   Object* set(size_t index, Object* o); //sets the object at index to be o, returns former object
   Object* remove(size_t index); //Replaces object at index with nullptr, returns objec
