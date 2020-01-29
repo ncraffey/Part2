1298a01c23d9832e6f93181c57a304509e6375a5
laposata <34639022+laposata@users.noreply.github.com>
2020-01-29 12:31:51 -0500

    updating array.h

--- Array.h
+++ Array.h
@@ -3,14 +3,23 @@ class Array: public Object{
 public:
   Array(size_t length, size_t data_length); // Creates an array of size length, where each object is data_length long
   ~Array();
-  virtual Object* get(size_t index); //returns the object at index
-  virtual Object* set(size_t index, Object* o); //sets the object at index to be o, returns former object
-  virtual Object* remove(size_t index); //Replaces object at index with nullptr, returns object
+  virtual void* get(size_t index); //returns the object at index
+  virtual void* set(size_t index, void* o); //sets the object at index to be o, returns former object
+  virtual void* remove(size_t index); //Replaces object at index with nullptr, returns object
   bool empty(); //checks if there are any items in the array
   size_t hash(); //returns the hash of the array
-  bool equals(Object* o); //checks if this is equal to o
+  bool equals(void* o); //checks if this is equal to o
   void clear(); // replaces all objects with nullpttr
   size_t length(); //returns the length of the array
+};
+
+
+class objArray: public Array{
+public:
+  Array(size_t length, size_t data_length); // Creates an array of size length, where each object is data_length long
+  virtual Object* get(size_t index); //returns the object at index
+  virtual Object* set(size_t index, Object* o); //sets the object at index to be o, returns former object
+  virtual Object* remove(size_t index); //Replaces object at index with nullptr, returns objec
 };
 
 class intArray: public Array{
